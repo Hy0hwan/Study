c51600d92f9a7fed8484b518b6fafdc4935d8b52
HyoHwan <gyghks1017@gmail.com>
2024-11-12 16:48:54 +0900

    [Feat] : 라우터 셋팅

--- react/weather-dashboard/src/App.tsx
+++ react/weather-dashboard/src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './views/Home';
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

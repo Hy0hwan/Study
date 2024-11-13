5a20f46107c471ebe0929ed311fd1fdfd1175cc8
HyoHwan <gyghks1017@gmail.com>
2024-11-14 00:38:25 +0900

    Design : Houry컴포넌트안 TimeWeather컴포넌트 화면넘침수정 및 스크롤바 숨김

--- weatherDashboard/weatherDashboard/src/components/common/Houry.tsx
+++ weatherDashboard/weatherDashboard/src/components/common/Houry.tsx
@@ -39,17 +39,37 @@ const Houry = () => {
       icons: '/src/assets/icons/1063d.svg',
       temp: '12',
     },
+    {
+      time: '1시',
+      icons: '/src/assets/icons/1063d.svg',
+      temp: '12',
+    },
+    {
+      time: '1시',
+      icons: '/src/assets/icons/1063d.svg',
+      temp: '12',
+    },
+    {
+      time: '1시',
+      icons: '/src/assets/icons/1063d.svg',
+      temp: '12',
+    },
+    {
+      time: '1시',
+      icons: '/src/assets/icons/1063d.svg',
+      temp: '12',
+    },
   ];
 
   return (
-    <Card className="bg-white flex-1 h-full max-w-[calc(50%-48px)]">
+    <Card className="bg-white flex-1 h-full max-w-[calc(50%-48px)] ">
       <CardHeader>
         <CardTitle className="text-xl">Houry</CardTitle>
         <CardDescription>
           오늘의 시간대별 날씨를 조회하고 있습니다.
         </CardDescription>
       </CardHeader>
-      <CardContent className="w-full flex items-start gap-4">
+      <CardContent className="w-full flex items-start gap-4 overflow-x-scroll">
         {timeWeatherData.map((value, index) => (
           <TimeWeather data={value} key={index} />
         ))}

8ed2eacdd1fd1f849b11a4ef8e2b44d847e86c4c
HyoHwan <gyghks1017@gmail.com>
2024-11-20 00:42:21 +0900

    feat : next-board ui 구현

---
+++ next-board/README.md
@@ -0,0 +1,2 @@
+1. React Markdown Editor 라이브러리 설치: `npm i @uiw/react-markdown-editor`
+2. 서버리스 데이터베이스 Supabase 설치: `npm install @supabase/supabase-js`
--- next/src/components/index.ts
+++ next/src/components/index.ts
@@ -1,3 +1,6 @@
-export { Button, buttonVariants } from './ui/button';
-export { Input } from './ui/input';
-export { SearchBar } from './ui/search-bar';
+export { Button, buttonVariants } from './ui/button/button';
+export { Input } from './ui/input/input';
+export { SearchBar } from './ui/search-bar/search-bar';
+export { Progress } from './ui/progress/progress';
+export { Calendar } from './ui/calendar/calendar';
+export { Popover, PopoverContent, PopoverTrigger } from './ui/popover/popover';

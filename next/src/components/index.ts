c249844d721ee5dcf5156407db6cd97b0bddef19
HyoHwan <gyghks1017@gmail.com>
2024-11-20 00:42:56 +0900

    remove : 내가한거 삭제..

--- next/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Button, buttonVariants } from './ui/button/button';
-export { Input } from './ui/input/input';
-export { SearchBar } from './ui/search-bar/search-bar';
-export { Progress } from './ui/progress/progress';
-export { Calendar } from './ui/calendar/calendar';
-export { Popover, PopoverContent, PopoverTrigger } from './ui/popover/popover';
